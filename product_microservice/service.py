52327b22405557c1ca0a4aee3bb3aebdb0a88a91
Sulivan Lominchar <sulominchar@gmail.com>
2025-08-11 16:20:42 -0400

    microservice product

--- product_microservice/service.py
+++ product_microservice/service.py
@@ -1,10 +1,32 @@
 import requests
+from flask import Flask, jsonify, request, make_response
+import jwt
+from functools import wraps
+import json
 import os
-from flask import Flask, jsonify
+from jwt.exceptions import DecodeError
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 port = int(os.environ.get('PORT', 5000))
+BASE_URL = "https://dummyjson.com" #the url to consume the api
 
+def token_required(f):
+    """
+    Decorate routes to require authentication.
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('token')
+        if not token:
+            return jsonify({'error': 'Authorization token is missing'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user_id = data['user_id']
+        except DecodeError:
+            return jsonify({'error': 'Authorization token is invalid'}), 401
+        return f(current_user_id, *args, **kwargs)
+    return decorated
 
 @app.route('/')
 def home():
@@ -14,10 +36,30 @@ def home():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
 
-BASE_URL = "https://dummyjson.com" #the url to consume the api
+
+with open('users.json', 'r') as f:
+    users = json.load(f)
+
+@app.route('/auth', methods=['POST'])
+def authenticate_user():
+    if request.headers['Content-Type'] != 'application/json':
+        return jsonify({'error': 'Unsupported Media Type'}), 415
+    username = request.json.get('username')
+    password = request.json.get('password')
+    for user in users:
+        if user['username'] == username and user['password'] == password:
+            token = jwt.encode({'user_id': user['id']}, app.config['SECRET_KEY'],algorithm="HS256")
+            response = make_response(jsonify({'message': 'Authentication successful'}))
+            response.set_cookie('token', token) #send the token in the cookies
+            return response, 200
+    return jsonify({'error': 'Invalid username or password'}), 401
+
+
 @app.route('/products', methods=['GET'])
-def get_products():
-    response = requests.get(f"{BASE_URL}/products")
+@token_required #calling custom decorator to handling authentication
+def get_products(current_user_id):
+    headers = {'Authorization': f'Bearer {request.cookies.get("token")}'} #taking the auth token from the cookies
+    response = requests.get(f"{BASE_URL}/products", headers=headers)
     if response.status_code != 200:
         return jsonify({'error': response.json()['bad request']}), response.status_code
     products = []
